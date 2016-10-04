15e159a4766f24673316909a62bb9153d5835150
Valentin Chernyarv <sergeich1997@yandex.ru>
2016-10-04 10:45:42 +0300

    git fix

---
+++ Упражнение №3.py
@@ -0,0 +1,39 @@
+﻿
+
+
+file_in = open("c:\sec.in", "r")
+
+count_num=int(file_in.readline())
+
+
+line=str(file_in.readline())
+file_in.close
+
+num=[]
+num_swap=[]
+
+num = line.split(' ')
+print ("Нужно обработать "+str(count_num)+" чисел")
+print ("Полная последовательность чисел:")
+print (num)
+
+
+if count_num%2==0:
+    count=int(count_num/2)
+    count=int((count_num-1)/2)
+for i in range(0,count,1):
+
+  num_swap.append(num[i])
+  num_swap.append(num[count_num-i-1])
+
+
+if count_num%2!=0: num_swap.append(num[count])
+
+
+
+print (' '.join(map(str, num_swap)))
+
+output = open('c:\sec.out', 'w')
+print (' '.join(map(str, num_swap)), file=output)
+output.close()
+
