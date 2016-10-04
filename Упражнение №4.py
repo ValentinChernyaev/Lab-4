7d05b8c4d7f4bbb7b0737ff48356881b9a0e7773
Valentin Chernyarv <sergeich1997@yandex.ru>
2016-10-04 11:04:52 +0300

    commit exercise 4

--- Упражнение №4.py
+++ Упражнение №4.py
@@ -1,5 +1,6 @@
 ﻿
 
+
 file_in = open("c:\sec.in", "r")
 
 
@@ -14,10 +15,10 @@ file_in.close
 Ryad=[]
 
 Ryad= line.split(' ')
+
 print ("Нужно обработать "+str(jumps)+" секунд(ы)")
 print ("Последовательность расстановки кузнечиков до прыжков:")
-print (' '.join(map(str, Ryad))) # Переделать в формат без запятых
-
+print (' '.join(map(str, Ryad)))
 
 len_ryad=len(Ryad)
 
@@ -25,8 +26,6 @@ len_ryad=len(Ryad)
 for i in range(1,jumps+1,1):
   jump=int(Ryad.pop())
   Ryad.insert((len_ryad-1-jump), jump)
-#------------------------------------------------------------------
-
 
 print ("Последовательность расстановки кузнечиков после прыжков:")
 print (' '.join(map(str, Ryad)))
